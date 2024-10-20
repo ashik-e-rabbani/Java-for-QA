b9afc9630274398663d49e88a0d98f84ba6ae599
Ashik Rabbani <160457486+ashikfoxtel@users.noreply.github.com>
2024-10-20 15:43:56 +0600

    Data Types

---
+++ Basics/DataTypes.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int age = 25;               // Integer data type
+        double price = 99.99;        // Double data type
+        char initial = 'A';         // Char data type
+        boolean isTestPassed = true; // Boolean data type
+
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Initial: " + initial);
+        System.out.println("Test Passed: " + isTestPassed);
+    }
+}
