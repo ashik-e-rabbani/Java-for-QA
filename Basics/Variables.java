9aa07fcd5f1847ade4df4e0d8f1d605459ce4ba2
Ashik Rabbani <160457486+ashikfoxtel@users.noreply.github.com>
2025-01-06 00:38:44 +0600

    Update Variables.java

--- Basics/Variables.java
+++ Basics/Variables.java
@@ -1,12 +1,23 @@
 package Basics;
 
 public class Variables {
+    static int sharedCounter = 0;  // Shared among all instances.
+
+    enum Day {
+        MONDAY, TUESDAY, WEDNESDAY
+    }
+
     public static void main(String[] args) {
         int age = 30;       // int variable
         String userName = "John";  // String variable
         int MAX_SALARY = 10000;
+        final int dob = 31101996;
+        int randomValue = (int) (Math.random() * 100);  // Initialized with a random number.
+        Day today = Day.MONDAY;
+
 
         System.out.println("Name: " + userName);
         System.out.println("Age: " + age +"\nSalary: " + MAX_SALARY);
     }
 }
+
